f03b4c4d302c629d8b91edf8b8407199f0874265
kofia894 <60142082+kofia894@users.noreply.github.com>
2021-12-02 16:30:59 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def index():
     # Use a breakpoint in the code line below to debug your script.
     return "Mini Project 4: Docker Based Web Application" # Press Ctrl+F8 to toggle the breakpoint.
 
-@app.route("/permissions/<code>", methods=['GET','POST'])
-def permissions(code):
+@app.route("/permissions", methods=['GET'])
+def permissions():
 
     permission = {'0': '[]',
                   '1': '[execute]',
@@ -23,47 +23,34 @@ def permissions(code):
                   '4': '[read]',
                   '5':'[read, execute]',
                   '6':'[read, write]',
-                  '7':'[read,write,execute]' }
+                  '7':'[read,write,execute]'}
 
     users = {0: 'owner',
              1: 'group',
              2: 'other'}
     i = 0
     out = {}
-    for j in code:
-        out[users[i]] = permission[j]
-        i += 1
 
-    if (request.method == 'GET'):
-        data = out
-        return jsonify({'data': data})
+    if 'code' in request.args:
+        code = request.args['code']
 
+        for j in code:
+            out[users[i]] = permission[j]
+            i += 1
 
-
-@app.route("/parity/<b1>&<b2>&<b3>&<b4>", methods=['GET','POST'])
-def parity(b1, b2, b3, b4):
-    bits = [b1, b2, b3, b4]
-
-    bit1 = []
-    bit2 = []
-
-    parity_bit = " "
-    j = 0
-    for i in range(0, len(bits)):
-        bit1[j] = i[0]
-        bit2[j] = i[1]
-        j += 1
-
-    k = 0
-    parity1 = bit1[k] ^ bit1[k+1] ^ bit1[k+2] ^ bit1[k+3]
-    parity2 = bit2[k] ^ bit2[k+1] ^ bit2[k+2] ^ bit2[k+3]
-
-    parity_bit += str(parity2) + str(parity1)
-
-    return parity_bit
+        return jsonify(out)
 
 
+@app.route("/parity", methods=['GET'])
+def parity():
+    bits = request.args.to_dict()
 
+    bit1 = 0
+
+    for i in bits:
+        bit1 = bit1 ^ int(bits[i])
+
+    return jsonify("Parity bit: " + str(bit1))
 
 if __name__ == "__main__":
     app.run(debug=True)
